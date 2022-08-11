76658c89c859391aa2e64567ce9be863c6b52415
Lørd <fernandesherick1232015@gmail.com>
2022-08-11 19:10:12 -0300

    front-end de pesquisa

---
+++ js/pesquisa.js
@@ -0,0 +1,15 @@
+// function pesquisar(){
+//     var filtro,input,menu,menuItens,links;
+//     input = document.getElementById("pesquisar");
+//     filtro = input.value.toUpperCase();
+//     menu = document.getElementById("menu");
+//     menuItens = document,getElementsByTagName("li");
+//     for(var i=0;i<menuItens.length;i++){
+//         links = menuItens[i].getElementsByTagName("a")[0];
+//         if(links.innerHTML.toUpperCase().indexOff(filtro)>-1){
+//             menuItens[i].style.display="";
+//         }else{
+//             menuItens[i].style.display="none";
+//         }
+//     }
+// }
